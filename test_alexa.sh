34f96d41e38c98942868daf5d95a98ebed698c88
pi3123 <unboxer04@gmail.com>
2017-05-20 23:45:12 -0400

    Add files via upload

---
+++ test_alexa.sh
@@ -0,0 +1,17 @@
+#Starting test engine
+
+espeak "Starting alexa compaitiblity test engine" 2>/dev/null
+sleep 1s
+espeak "Testing Microphone" 2>/dev/null
+sleep 2s
+espeak "Input OK!" 2>/dev/null
+espeak "Testing Network Connection" 2>/dev/null
+sleep 2s
+espeak "Connecting to Google" 2>/dev/null
+sleep 3s
+espeak "Data Recieved with 0 percent loss" 2>/dev/null
+espeak "Starting Alexa Sample app" 2>/dev/null
+sleep 1s
+
+cd ~
+. wake_alexa.sh
---
+++ wake_alexa.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+espeak "Starting Companion Service" 2>/dev/null
+cd /home/pi/Desktop/alexa-avs-sample-app/samples sleep 2s
+cd companionService && npm start&
+
+sleep 4s
+espeak "Connecting to Amazon Developer Site" 2>/dev/null
+
+cd /home/pi/Desktop/alexa-avs-sample-app/samples
+cd javaclient && mvn exec:exec&
+
+sleep 22.5s
+espeak "Starting Wake Word Engine" 2>/dev/null
+
+cd /home/pi/Desktop/alexa-avs-sample-app/samples
+cd wakeWordAgent/src && ./wakeWordAgent -e sensory &
+
+sleep 2s
+espeak "Alexa is Running and waiting for your commands" 2>/dev/null
